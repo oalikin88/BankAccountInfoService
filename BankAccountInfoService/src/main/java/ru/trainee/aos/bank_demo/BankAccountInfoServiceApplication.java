19d77b7becb94a185e93fbdb90d68202d81d7827
oalikin88 <oalikin88@desktop>
2022-03-25 10:13:36 +0300

    Создание проекта

---
+++ BankAccountInfoService/src/main/java/ru/trainee/aos/bank_demo/BankAccountInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.trainee.aos.bank_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountInfoServiceApplication.class, args);
+	}
+
+}
